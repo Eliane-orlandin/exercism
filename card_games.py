cf12706d7a555a13d81ef7b9a14082a2159b1360
Eliane <liorlandin@gmail.com>
2023-06-05 12:53:15 -0300

    Usando a o método de concatenar em uma lista.

--- card_games.py
+++ card_games.py
@@ -12,3 +12,12 @@ def get_rounds(number):
     """
     round_number = number
     return [round_number, round_number + 1, round_number + 2]
+
+def concatenate_rounds(rounds_1, rounds_2):
+    """Concatenate two lists of round numbers.
+
+    :param rounds_1: list - first rounds played.
+    :param rounds_2: list - second set of rounds played.
+    :return: list - all rounds played.
+    """ 
+    return rounds_1 + rounds_2
